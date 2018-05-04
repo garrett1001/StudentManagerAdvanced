fa6e402ea039b71bb01f485c392bb92cbf558962
garrett1001 <gwelton@mail.csuchico.edu>
2018-05-03 17:32:14 -0700

    complete

--- student.cpp
+++ student.cpp
@@ -1,5 +1,4 @@
 #include "student.h"
-#include <string>
 
 void student::setName(std::string first, std::string last)
 {
@@ -10,9 +9,33 @@ void student::setName(std::string first, std::string last)
 std::string student::fullName()
 {
     std::string full;
+
     full.append(mFirst);
     full.append(" ");
     full.append(mLast);
 
         return full;
+}
+
+void student::addGrade(double grade)
+{
+    mGrades.push_back(grade);
+}
+
+double student::getScore()
+{
+    double score = 0;
+
+
+    for(int i=0; i < mGrades.size(); i++)
+    {
+        score = score + mGrades[i];
+    }
+    
+    if(score > 0)
+    {
+        score = score / mGrades.size();
+    }
+
+        return score;
 }
--- student.h
+++ student.h
@@ -1,16 +1,20 @@
 #ifndef _STUDENT_H_
 #define _STUDENT_H_
 #include <string>
+#include <vector>
 
 class student
 {
     private:
     std::string mFirst;
     std::string mLast;
+    std::vector<double> mGrades;
 
     public:
     void setName(std::string,std::string);
     std::string fullName();
+    void addGrade(double);
+    double getScore();
 
 };
 
